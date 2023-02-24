05b87e1ad2f1fc7a8f40d3f8b664a4aca9ca8049
duan <duannguyen242@gmail.com>
2023-02-24 13:35:19 +0700

    demo

---
+++ webMVC/Controllers/bookCrudController.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+using webMVC.Models;
+
+namespace webMVC.Controllers
+{
+    public class bookCrudController : Controller
+    {
+
+        HttpClient client = new HttpClient();
+
+        // GET: /bookCrud
+        [HttpGet]
+        public ActionResult Index()
+        {
+            List<book> b = new List<book>();
+            client.BaseAddress = new Uri(" https://localhost:44301/api/booksAPI");
+            var response = client.GetAsync("booksAPI");
+            response.Wait();
+            var test = response.Result;
+            if (test.IsSuccessStatusCode)
+            {
+                var display = test.Content.ReadAsAsync<List<book>>();
+                display.Wait();
+                b = display.Result;
+            }
+            return View(b);
+        }
+
+        // POST: /bookCrud/Delete/{id}
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            client.BaseAddress = new Uri(" https://localhost:44301/api/booksAPI");
+            client.DeleteAsync("booksAPI/" + id.ToString());
+            return RedirectToAction("Index");
+        }
+
+        // GET: /bookCrud/create
+        [HttpGet]
+        public ActionResult create()
+        {
+            return View("create");
+        }
+
+        // GET: /bookCrud/update/{id}
+        [HttpGet]
+        public ActionResult Update(int id)
+        {
+            book b = null;
+            client.BaseAddress = new Uri(" https://localhost:44301/api/booksAPI");
+            var response = client.GetAsync("booksAPI/" + id.ToString());
+            response.Wait();
+            var test = response.Result;
+            if (test.IsSuccessStatusCode)
+            {
+                var display = test.Content.ReadAsAsync<book>();
+                display.Wait();
+                b = display.Result;
+            }
+            return View(b);
+        }
+
+
+        // GET: /bookCrud/update/{id}
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            book b = null;
+            client.BaseAddress = new Uri("https://localhost:44301/api/booksAPI");
+            var response = client.GetAsync("booksAPI/" + id.ToString());
+            response.Wait();
+            var test = response.Result;
+            if (test.IsSuccessStatusCode)
+            {
+                var display = test.Content.ReadAsAsync<book>();
+                display.Wait();
+                b = display.Result;
+            }
+            return View(b);
+        }
+
+
+        // PUT: /bookCrud/update/[body]
+        [HttpPost]
+        public ActionResult Edit(book b)
+        {
+          
+            client.BaseAddress = new Uri("https://localhost:44301/api/booksAPI");
+            var response = client.PutAsJsonAsync($"booksAPI",b);
+            response.Wait();
+            var test = response.Result;
+            if (test.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View("Edit");
+        }
+
+
+        // POST: /bookCrud/create/
+        [HttpPost]
+        public ActionResult create(book b)
+        {
+            client.BaseAddress = new Uri(" https://localhost:44301/api/booksAPI");
+            var response = client.PostAsJsonAsync("booksAPI",b);
+            response.Wait();
+            var test = response.Result;
+            if (test.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View("create");
+        }
+
+        
+    }
+}
---
+++ webMVC/Controllers/booksAPIController.cs
@@ -0,0 +1,87 @@
+﻿
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using webMVC.Models;
+using HttpDeleteAttribute = System.Web.Http.HttpDeleteAttribute;
+using HttpGetAttribute = System.Web.Http.HttpGetAttribute;
+using HttpPostAttribute = System.Web.Http.HttpPostAttribute;
+using HttpPutAttribute = System.Web.Http.HttpPutAttribute;
+
+namespace webMVC.Controllers
+{ 
+        public class booksAPIController : ApiController
+        {
+            private bookEntities db = new bookEntities();
+
+            // GET: api/booksAPI
+            [HttpGet]
+            public IEnumerable<book> getList()
+            {
+                return db.books.ToList();
+            }
+
+            // GET: api/booksAPI/{id}
+            [HttpGet]
+            public IHttpActionResult getBook(int id)
+            {
+                book b = db.books.Find(id);
+                if (b == null)
+                {
+                    return NotFound();
+                }
+                return Ok(b);
+            }
+
+            // POST: api/booksAPI
+            [HttpPost]
+            public IHttpActionResult createBook([FromBody] book b)
+            {
+            book bo = db.books.Find(b.book_id);
+            if (bo != null)
+            {
+                return NotFound();
+            }
+                db.books.Add(b);
+                db.SaveChanges();
+
+                return Ok(b);
+            }
+
+            // PUT: api/booksAPI/[body]
+            [HttpPut]
+            public IHttpActionResult update([FromBody] book b)
+            {
+                book bo = db.books.Find(b.book_id);
+                if (bo == null)
+                {
+                    return NotFound();
+                }
+                //update
+                bo.book_id = b.book_id;
+                bo.title = b.title;
+                bo.price = b.price;
+                bo.author = b.author;
+                bo.publish_date = b.publish_date;
+                db.SaveChanges();
+
+                return Ok(bo);
+
+            }
+
+            // DELETE: api/booksAPI/{id}
+            [HttpDelete]
+            public IHttpActionResult delete(int id)
+            {
+                book bo = db.books.Find(id);
+                if (bo == null)
+                {
+                    return NotFound();
+                }
+                db.books.Remove(bo);
+                db.SaveChanges();
+                return Ok();
+            }
+        }
+    }
+
